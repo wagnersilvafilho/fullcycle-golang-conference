dcd381e898870a1159ea7c394a8ce96ce9ec9b24
Wagner Filho <wagnersf97@gmail.com>
2024-03-29 14:43:32 -0300

    melhorias de loop e separacao em classes de funcoes

--- main.go
+++ main.go
@@ -7,6 +7,9 @@ import (
 	"time"
 )
 
+const delay = 2
+const iterador = 1
+
 type Person struct {
 	name string
 	age  int
@@ -38,11 +41,49 @@ func listenWebServer() {
 	http.ListenAndServe(":8000", nil)
 }
 
+func testSite(sites []string) {
+	for i := 0; i < iterador; i++ {
+		for _, site := range sites {
+			resp, _ := http.Get(site)
+			if resp.StatusCode == 200 {
+				fmt.Println("Site:", site, "foi carregado com sucesso!")
+			} else {
+				fmt.Println("Site:", site, "retornou status diferente de 200. Status code:", resp.StatusCode)
+			}
+		}
+		fmt.Println(i+1, "ª validação realizada.")
+		time.Sleep(delay * time.Second)
+	}
+}
+
 func checkPageUp() {
 	fmt.Println("Enviando requisição de teste...")
-	site := "https://google.com"
-	resp, _ := http.Get(site)
-	fmt.Println(resp.Status)
+	sites := []string{"https://google.com", "https://g1.globo.com", "https://tray.com.br"}
+	testSite(sites)
+}
+
+func quit() {
+	fmt.Println("Bye...")
+	os.Exit(0)
+}
+
+func returnToMenu(opcao string) {
+	fmt.Println("\nDigite uma opção: ")
+	fmt.Println("1- Voltar ao menu anterior")
+	fmt.Println("0- Sair")
+	fmt.Scan(&opcao)
+	fmt.Println("A opção escolhida foi", opcao)
+	if opcao == "0" {
+		quit()
+	}
+}
+
+func printMenu() {
+	fmt.Println("\n1- Iniciar contador")
+	fmt.Println("2- Iniciar servidor web")
+	fmt.Println("3- Iniciar validação de sites")
+	fmt.Println("0- Sair do programa")
+	fmt.Println("Digite uma opção: ")
 }
 
 func main() {
@@ -56,28 +97,26 @@ func main() {
 	fmt.Println(pessoa)
 	pessoa.Walk()
 	fmt.Println("Olá ", pessoa.name)
-	fmt.Println("1- Iniciar contador")
-	fmt.Println("2- Iniciar servidor web")
-	fmt.Println("0- Sair do programa")
 	for {
-		fmt.Println("Digite uma opção: ")
+		printMenu()
 		fmt.Scan(&opcao)
 		fmt.Println("A opção escolhida foi", opcao)
 		switch opcao {
 		case "1":
 			contador(3)
+			returnToMenu(opcao)
 			break
 		case "2":
 			listenWebServer()
 			break
 		case "3":
 			checkPageUp()
-			os.Exit(0)
+			returnToMenu(opcao)
+			break
 		case "0":
-			fmt.Println("Saindo do programa...")
-			os.Exit(0)
+			quit()
 		default:
-			fmt.Println("Opção inválida.")
+			fmt.Print("\nOpção inválida.\n\n")
 		}
 	}
 }
