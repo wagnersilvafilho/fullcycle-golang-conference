ffde8d1ab6908204c86c06e322fa7b7bf253d61e
Wagner Filho <wagnersf97@gmail.com>
2024-07-30 15:41:34 -0300

    aula aprenda go lang - conceitos de packages

---
+++ users/user.go
@@ -0,0 +1,17 @@
+package users
+
+type user struct {
+	Name string
+	Age  uint8
+}
+
+func New(name string, age uint8) user {
+	return user{
+		Name: name,
+		Age:  age,
+	}
+}
+
+func AddYear(u *user) {
+	u.Age++
+}
