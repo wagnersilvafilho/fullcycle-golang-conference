a51039f94ae3ba4b27b1f64c30ab40b503da80cd
Wagner Filho <wagnersf97@gmail.com>
2024-08-13 11:15:58 -0300

    aula aprenda go lang - provider bucket

---
+++ internal/bucket/bucket.go
@@ -0,0 +1,49 @@
+package bucket
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"reflect"
+)
+
+const (
+	AwsProvider BucketType = iota
+)
+
+type BucketType int
+
+type BucketInterface interface {
+	Upload(io.Reader, string) error
+	Download(string, string) (*os.File, error)
+	Delete(string) error
+}
+
+type Bucket struct {
+	p BucketInterface
+}
+
+func (b *Bucket) Upload(file io.Reader, key string) error {
+	return b.p.Upload(file, key)
+}
+
+func (b *Bucket) Download(src, dst string) (file *os.File, err error) {
+	return b.p.Download(src, dst)
+}
+
+func (b *Bucket) Delete(key string) error {
+	return b.p.Delete(key)
+}
+
+func New(bt BucketType, cfg any) (b *Bucket, err error) {
+	rt := reflect.TypeOf(cfg)
+	switch bt {
+	case AwsProvider:
+		//TODO: implementar aws provider
+
+	default:
+		return nil, fmt.Errorf("type not implemented")
+	}
+
+	return
+}
