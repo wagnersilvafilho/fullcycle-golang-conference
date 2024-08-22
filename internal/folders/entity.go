116f433577badce9f080951f6e5b6d8140de241e
Wagner Filho <wagnersf97@gmail.com>
2024-08-21 21:54:25 -0300

    aula 12 aprenda golang - finaliza api usuario e inicia api folder

---
+++ internal/folders/entity.go
@@ -0,0 +1,37 @@
+package folders
+
+import (
+	"errors"
+	"time"
+)
+
+var (
+	ErrNameRequired = errors.New("name is required")
+)
+
+type Folder struct {
+	ID         int64     `json:"id"`
+	ParentID   int64     `json:"parent_id"`
+	Name       string    `json:"name"`
+	CreatedAt  time.Time `json:"created_at"`
+	ModifiedAt time.Time `json:"modified_at"`
+	Deleted    bool      `json:"-"`
+}
+
+func New(name string, parentID int64) (*Folder, error) {
+	f := Folder{Name: name, ParentID: parentID, ModifiedAt: time.Now()}
+
+	err := f.Validate()
+	if err != nil {
+		return nil, err
+	}
+
+	return &f, nil
+}
+
+func (f *Folder) Validate() error {
+	if f.Name == "" {
+		return ErrNameRequired
+	}
+	return nil
+}
